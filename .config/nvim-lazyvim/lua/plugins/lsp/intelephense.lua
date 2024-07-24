880969a0b5ada509aab3457c04d0e8ea758b2086
max <svonjoi@gmail.com>
2024-07-24 14:38:55 +0200

    intellephense: ignore .dist in root folder

--- .config/nvim-lazyvim/lua/plugins/lsp/intelephense.lua
+++ .config/nvim-lazyvim/lua/plugins/lsp/intelephense.lua
@@ -15,6 +15,21 @@ return {
               files = {
                 associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
                 maxSize = 5000000,
+                exclude = {
+                  -- дефолтная дрисня
+                  "**/.git/**",
+                  "**/.svn/**",
+                  "**/.hg/**",
+                  "**/CVS/**",
+                  "**/.DS_Store/**",
+                  "**/node_modules/**",
+                  "**/bower_components/**",
+                  "**/vendor/**/{Tests,tests}/**",
+                  "**/.history/**",
+                  "**/vendor/**/vendor/**",
+                  -- от меня
+                  "**/.dist",
+                },
               },
               environment = {
                 phpVersion = "7.4",
