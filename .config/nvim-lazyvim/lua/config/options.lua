9e4a138b1a47facbf71d474d453d43ebb8fc8819
max <svonjoi@gmail.com>
2025-04-06 01:43:36 +0300

    remove session restore fix cuz it broke neotree

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -7,6 +7,8 @@
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+-- vim.g.lazyvim_cmp = "nvim-cmp"
+
 -- theme
 vim.opt.background = "dark" -- set this to dark or light
 -- vim.cmd("colorscheme oxocarbon")
@@ -14,7 +16,9 @@ vim.opt.background = "dark" -- set this to dark or light
 
 -- https://github.com/jedrzejboczar/possession.nvim/issues/19
 -- vim.o.sessionoptions = "buffers,curdir,tabpages,winsize,help,globals,skiprtp,folds"
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "*" },
-  command = "normal zx",
-})
+vim.o.sessionoptions = "buffers,curdir,tabpages,winsize,help,globals,skiprtp"
+-- UOD. фолдит папки в neotree после удаления файла/папки
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   pattern = { "*" },
+--   command = "normal zx",
+-- })
