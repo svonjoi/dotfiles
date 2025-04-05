4c3c988f50504ff9d75488b8adb7ed0801051d03
max <svonjoi@gmail.com>
2025-04-06 01:47:42 +0300

    add telescope tabs

--- .config/nvim-lazyvim/lua/plugins/telescope.lua
+++ .config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -61,4 +61,17 @@ return {
       }
     end,
   },
+  {
+    "LukasPietzschmann/telescope-tabs",
+    config = function()
+      require("telescope").load_extension("telescope-tabs")
+      require("telescope-tabs").setup({
+        -- Your custom config :^)
+      })
+    end,
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+      { "<leader><tab>e", "<cmd>Telescope telescope-tabs list_tabs<cr>", desc = "List tabs" },
+    },
+  },
 }
