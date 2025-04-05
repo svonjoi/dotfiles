10e5c0a335f4fb4cb1ca04d24edb3ab6ebad1563
max <svonjoi@gmail.com>
2025-04-06 01:46:43 +0300

    dadbod

--- .config/nvim-lazyvim/lua/plugins/code/database.lua
+++ .config/nvim-lazyvim/lua/plugins/code/database.lua
@@ -1,5 +1,25 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+  -- WARNING option --ssl-verify-server-cert is disabled, because of an insecure passwordless login.
+  -- mariadb --ssl-verify-server-cert=false ...
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-completion",
+  },
 }
