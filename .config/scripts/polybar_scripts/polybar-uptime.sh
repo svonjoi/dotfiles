44136acb40355692adf8451a0176cdc039cf37ba
max <svonjoi@gmail.com>
2025-05-15 14:36:53 +0300

    minor changes in polybar scripts

--- .config/scripts/polybar_scripts/polybar-uptime.sh
+++ .config/scripts/polybar_scripts/polybar-uptime.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
+v="${COLOR_SPECIAL_TEXT}"
+
 # days="$($HOME/.config/scripts/uptime_seconds.sh) / 86400" | bc
 seconds="$($HOME/.config/scripts/uptime_seconds.sh)"
 days=$(echo "$seconds / 86400" | bc)
-echo "$days"
+
+# 󰤄󰤆
+echo "%{F${v}}󰤆%{F-} $days"
 
